921f15f7a65ff6c80e6e3aeda714a2b912aa00e4
Abdulla Al Masud <info.abdullaalmasudbd@gmail.com>
2025-05-05 00:59:41 +0600

    add file

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'jakarta': ['"Plus Jakarta Sans"', 'serif']
+      },
+      screens: {
+        ml: '840px'
+      }
+    },
+  },
+  plugins: [],
+}
+
